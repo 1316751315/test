5b66966398040288fb6519e8f01f01d5ef1a1f6b
aa <jcshan@bios.zd-tech.com.cn>
2022-12-08 09:22:28 +0800

    apml message

---
+++ coreTypes.h
@@ -0,0 +1,9 @@
+#ifndef COMDEF_H
+#define COMDEF_H
+typedef char CHAR;
+typedef unsigned long int uint32;   /* Unsigned 32 bit value */
+typedef unsigned short uint16;      /* Unsigned 16 bit value */
+typedef unsigned char uint8;        /* Unsigned 8 bit value */
+typedef signed long int int32;      /* Signed 32 bit value */
+typedef uint8 u8;
+#endif
